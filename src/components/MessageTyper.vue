31f5e27e3c96d68f7cb9e98e0c588f8a9762e8dd
Pablo Abril <pablo.abril@cybelangel.com>
2020-12-07 16:00:44 +0100

    add auto scrolling

--- src/components/MessageTyper.vue
+++ src/components/MessageTyper.vue
@@ -1,55 +1,114 @@
 <script>
 const MESSAGES = [
-    {content: 'Salut Flo\n', delay: 50},
-    {content: 'Salut ça va\n', delay: 3000},
-    {content: 'Salut ça va\n', delay: 4000},
-    {content: 'Salut ça va\n', delay: 5000},
-]
+  { content: "Salut Flo\n", delay: 50 },
+  { content: "Salut ça va\n", delay: 3000 },
+  { content: "Salut ça va\n", delay: 4000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 },
+  { content: "Salut ça va\n", delay: 5000 }
+];
 
 export default {
-    name: 'MessageTyper',
-    data() {
-        return {
-            messages: MESSAGES,
-        }
-    },
-    methods: {
-        onComplete() {
-            return
-        }
+  name: "MessageTyper",
+  data() {
+    return {
+      messages: MESSAGES
+    };
+  },
+  computed: {
+    messagesAutoDelay() {
+      return this.messages.map((x, index) => {
+        return { ...x, delay: index * 100 };
+      });
     }
-}
+  },
+  methods: {
+    onTyped() {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+  }
+};
 </script>
 
 <template>
-    <div id="messageContent">
-        <h3>Message Typer component</h3>
-        
-        <vue-typer
-            v-for="message in messages"
-            :key="message.id"
-            :text="message.content"
-            :pre-erase-delay='250000'
-            :pre-type-delay='message.delay'
-            caret-animation='blink'></vue-typer>
-        <!-- <vue-typer :text="message" :erase-on-complete="false" type-delay='70'></vue-typer><br> -->
-    </div>
+  <div id="messageContent">
+    <h3>Message Typer component</h3>
+
+    <vue-typer
+      v-for="message in messagesAutoDelay"
+      :key="message.id"
+      :text="message.content"
+      :pre-erase-delay="250000"
+      :pre-type-delay="message.delay"
+      @typed="onTyped"
+      caret-animation="blink"
+    ></vue-typer>
+    <!-- <vue-typer :text="message" :erase-on-complete="false" type-delay='70'></vue-typer><br> -->
+  </div>
 </template>
 
 <style lang="css">
 #messageContent {
-    text-align: left;
+  text-align: left;
 }
 
 .custom.caret {
-    display: none;
+  display: none;
 }
-
-.custom.caret.pre-erase {
-    display: none;
-}
-.custom.caret.pre-type {
-    display: none;
-}
-
 </style>
--- src/views/Flo.vue
+++ src/views/Flo.vue
@@ -1,25 +1,20 @@
 <script>
 import MessageTyper from "@/components/MessageTyper.vue";
 
-
 export default {
-    name: 'flo',
-    components: {
-        MessageTyper,
-    }
-}
+  name: "flo",
+  components: {
+    MessageTyper
+  }
+};
 </script>
 
 <template>
-    <div id="flo">
-        <h1>hello Flo</h1>
+  <div id="flo">
+    <h1>hello Flo</h1>
 
-        <message-typer />
-    </div>
+    <message-typer />
+  </div>
 </template>
 
-<style>
-.custom.caret.pre-erase {
-    display: none;
-}
-</style>
+<style></style>
